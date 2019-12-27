8bbed5099babb35c01636999e0a29b4391d2eb05
Oliver Huang <oliver@outlook.com>
2019-12-27 01:43:42 -0500

    Added Customer.java

--- Customer.java
+++ Customer.java
@@ -3,22 +3,113 @@ public class Customer extends User
    protected Order[] orderHistory;
    protected Wallet wallet;
    protected Cart cart;
+   protected int positionX;
+   protected int positionY;
    
    // Constructor ////////////////////////////////
-   public Customer (int id, String name, String username, String password)
+   /*
+      PARAMETERS:    N/A
+      RETURN VALUE:  N/A
+      PURPOSE:       Creat Customer object
+   */
+   public Customer (String name, String username, String password)
    {
-      super (id, name, username, password);
+      super (name, username, password);
    }
    
    // Functions /////////////////////////////////
-   public void addCard (String cardNumber, String expiryMonth, String expiryYear, String CVV)
+   /*
+      PARAMETERS:    Orders in database
+      RETURN VALUE:  Orders belongs to customer
+      PURPOSE:       Find all orders belongs to this customer
+   */
+   public Order[] getAllOrders (Order[] orders)
    {
-      Card c = new Card (cardNumber, expiryMonth, expiryYear, CVV);
-      wallet = new Wallet (c, 50);
-   }
-   ///////////////////////////
-   public void order (cart)
-   {
+      orderHistory = new Order[50];
+      int count = 0;
       
+      for (int i = 0; i < orders.length; i++)
+      {
+         if (orders.getCustomer() == this.username)
+         {
+            orderHistory[count] = orders[i];
+            count++;
+         }
+      }
+      return orderHistory;
+   }
+   //////////////////////////////////////////////////
+   /*
+      PARAMETERS:    Wallet
+      RETURN VALUE:  void
+      PURPOSE:       Add wallet to customer
+   */
+   public void setWallet (Wallet wallet)
+   {
+      this.wallet = wallet;
+   }
+   ///////////////////////////////////////////////////
+   /*
+      PARAMETERS:    amount want to add
+      RETURN VALUE:  void
+      PURPOSE:       Add balance to customer's account
+   */
+   public void addBalance (double amount)
+   {
+      if (wallet.hasCard())
+      {
+         wallet.balance = wallet.balance + amount;
+      }
+   }
+   ////////////////////////////////////////////////////
+   /*
+      PARAMETERS:    positionX, positionY
+      RETURN VALUE:  void
+      PURPOSE:       Let customer set their location
+   */
+   public void addPosition (int positionX, int positionY)
+   {
+      this.positionX = positionX;
+      this.positionY = positionY;
+   }
+   /////////////////////////////////////////////////////
+   /*
+      PARAMETERS:    N/A
+      RETURN VALUE:  Orders in delivery
+      PURPOSE:       Show customer's procceding orders
+   */
+   public Order[] getActiveOrders()
+   {
+      Order[] activeOrders = new Order[orderHistory.length];
+      int count = 0;
+      for (int i = 0; i < orderHistory.length; i++)
+      {
+         if(!orderHistory[i].isComplete())
+         {
+            activeOrders[count] = orderHistory[i];
+            count++;
+         }
+      }
+      return activeOrders;
+   }
+   ////////////////////////////////////////////////
+   /*
+      PARAMETERS:    order
+      RETURN VALUE:  void
+      PURPOSE:       Add new order to customer's order history
+   */
+   public void addOrder (Order order)
+   {
+      orderHistory.add(order);
+   }
+   //////////////////////////////////////////////////////
+   /*
+      PARAMETERS:    password
+      RETURN VALUE:  boolean
+      PURPOSE:       Check if password meets requirments
+   */
+   public String toString ()
+   {
+      return name + "/n" + username + "/n" + wallet.balance + "/n" + "Customer";
    }
 }
--- User.java
+++ User.java
@@ -1,9 +1,21 @@
+/*
+   CLASS NAME: User
+   AUTHOR:     Oliver Huang
+   DATE:       12/26/2019
+   SCHOOL:     AY Jackson Secondary School
+   PURPOSE:    A class that stores each user's information
+*/
+
 public class User extends Person
 {
    protected String username;
    protected String password;
    
    // Constructor //////////////////////////////////////////////////////////////////
+   /*
+      PARAMETERS:    N/A
+      PURPOSE:       Creat User object
+   */
    public User (String name, String username, String password)
    {
       super (name);
@@ -12,16 +24,31 @@ public class User extends Person
    }
    
    // Functions //////////////////////////////////////////////////////
+   /*
+      PARAMETERS:    username
+      RETURN VALUE:  boolean
+      PURPOSE:       Check if username meets requirments
+   */
    public boolean isUsernameValid (String username)
    {
       return username.length() < 16;
    }
    /////////////////////////////////////////
+   /*
+      PARAMETERS:    password
+      RETURN VALUE:  boolean
+      PURPOSE:       Check if password meets requirments
+   */
    public boolean isPasswordValid (String password)
    {
       return password.length() < 6;
    }
    //////////////////////////////////////////
+   /*
+      PARAMETERS:    username, password
+      RETURN VALUE:  boolean
+      PURPOSE:       Check if password-input matches password-in-file
+   */
    public boolean checkAuth (String username, String password)
    {
       if (username == this.username)
@@ -36,6 +63,11 @@ public class User extends Person
       return false;
    }
    ///////////////////////////////////////////////
+   /*
+      PARAMETERS:    username
+      RETURN VALUE:  void
+      PURPOSE:       Update username
+   */
    public void changeUsername (String username)
    {
       if (isUsernameValid(username))
@@ -44,6 +76,11 @@ public class User extends Person
       }
    }
    ///////////////////////////////////////////////
+   /*
+      PARAMETERS:    password
+      RETURN VALUE:  void
+      PURPOSE:       Update password
+   */
    public void changePassword (String password)
    {
       if (isPasswordValid(password))
@@ -52,6 +89,11 @@ public class User extends Person
       }
    }
    /////////////////////////////////////////
+   /*
+      PARAMETERS:    N/A
+      RETURN VALUE:  String
+      PURPOSE:       Output user info
+   */
    public String toString ()
    {
       return name + username;
