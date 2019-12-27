eb942670b2c394898edad79c4931f5d37392a53a
Abdullah Shahid <abdullaaaah_@outlook.com>
2019-12-26 22:03:48 -0500

    Driver class done except getDistance method

--- Driver.java
+++ Driver.java
@@ -1,15 +1,53 @@
 public class Driver extends Person
 {
    protected String phoneNumber;
-   protected Car car;
-   protected Order curOrder;
-   protected Location location;
+   protected String description;
+   protected int positionX;
+   protected int positionY;
+
    
-   // Constructor /////////////////////////////////////////////
-   public Driver (String phoneNumber, Car car, Location location)
+   public Driver (String name, String phoneNumber, String description, int positionX, int positionY)
    {
+         super(name);
          this.phoneNumber = phoneNumber;
-         this.car = car;
-         this.location = location; //test
+         this.description = description;
+         this.positionX = positionX; 
+         this.positionY = positionY;
+   }
+   
+   public String getPhoneNumber()
+   {
+      return this.phoneNumber;
+   }
+   
+   public String getDescription()
+   {
+      return this.description;
+   }
+   
+   public int getPositionX()
+   {
+      return this.positionX;
+   }
+   
+   public int getPositionY()
+   {
+      return this.positionY;
+   }
+   
+   public void updatePosition(int x, int y)
+   {
+      this.positionX = x;
+      this.positionY = y;
+   }
+   
+   public double getDistance(Map map, int x, int y)
+   {
+      return 0;
+   }
+   
+   public String toString()
+   {
+      return super.getName() + " Contact Number: " + this.phoneNumber + " Description: " + this.description;
    }
 }
