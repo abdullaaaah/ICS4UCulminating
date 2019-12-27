2e0ea557c572a6375c9eec0823b8ce3b4dd219e9
Oliver Huang <oliver@outlook.com>
2019-12-26 23:11:46 -0500

    Added User Class

--- Person.java
+++ Person.java
@@ -2,16 +2,23 @@ public abstract class Person
 {
    protected String name;
    
+   // Constructor ////////////////////////////////////////////
    public Person (String name)
    {
       this.name = name;
    }
    
+   // Functions ////////////////////////////////////////////////
    public void setName(String name)
    {
       this.name = name;
    }
-   
+   ///////////////////////////////////////////
+   public String getName()
+   {
+      return name;
+   }
+   ////////////////////////////////////////
    public abstract String toString();
 
 }
--- User.java
+++ User.java
@@ -4,10 +4,56 @@ public class User extends Person
    protected String password;
    
    // Constructor //////////////////////////////////////////////////////////////////
-   public User (int id, String name, String username, String password)
+   public User (String name, String username, String password)
    {
-      super (id, name);
+      super (name);
       this.username = username;
       this.password = password;
+   }
+   
+   // Functions //////////////////////////////////////////////////////
+   public boolean isUsernameValid (String username)
+   {
+      return username.length() < 16;
+   }
+   /////////////////////////////////////////
+   public boolean isPasswordValid (String password)
+   {
+      return password.length() < 6;
+   }
+   //////////////////////////////////////////
+   public boolean checkAuth (String username, String password)
+   {
+      if (username == this.username)
+      {
+         String temp = "";
+         for (int i = 0; i < password.length(); i++)
+         {
+            temp = temp + (password.charAt(i) + 5);
+         }
+         return temp == this.password;
+      }
+      return false;
+   }
+   ///////////////////////////////////////////////
+   public void changeUsername (String username)
+   {
+      if (isUsernameValid(username))
+      {
+         this.username = username;
+      }
+   }
+   ///////////////////////////////////////////////
+   public void changePassword (String password)
+   {
+      if (isPasswordValid(password))
+      {
+         this.password = password;
+      }
+   }
+   /////////////////////////////////////////
+   public String toString ()
+   {
+      return name + username;
    }
 }
